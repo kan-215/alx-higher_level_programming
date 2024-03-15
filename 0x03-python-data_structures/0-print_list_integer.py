c32e08f0854b20e1b8eca9ce27bb847f2fa8a1b8
kan-215 <katamialvin369@gmail.com>
2024-03-15 10:14:59 +0300

    0x02. Python - import & modules

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Print all integers."""
+
+
+def print_list_integer(my_list=[]):
+    """Take in a list and print all int."""
+    for i in my_list:
+        print(('{}').format(i))
