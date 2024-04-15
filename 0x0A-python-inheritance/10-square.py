44b7b77b6a370ab4326439085873df8ba0777d97
kan-215 <katamialvin369@gmail.com>
2024-04-15 23:50:35 +0300

    0x0A-python-inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Module implementing Rectangle and Square classes"""
+"""Module 9-rectangle"""
 
-from typing import Union
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
@@ -9,29 +8,30 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle class"""
 
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, width, height):
         """Initialize Rectangle"""
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
-    def area(self) -> int:
+    def area(self):
         """Return the area of Rectangle"""
         return self.__width * self.__height
 
-    def __str__(self) -> str:
+    def __str__(self):
         """Return Rectangle description"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
     """Square class"""
 
-    def __init__(self, size: int) -> None:
+    def __init__(self, size):
         """Initialize Square"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
 
-    def __str__(self) -> str:
+    def __str__(self):
         """Return Square description"""
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
