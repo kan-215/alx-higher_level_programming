4f644c3ccd7aba1c3bb606338761a3366efd53fd
kan-215 <katamialvin369@gmail.com>
2024-04-15 23:43:56 +0300

    0x0A-python-inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,36 +2,48 @@
 """Module 9-rectangle"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+from typing import Union
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self) -> None:
+        """Placeholder for area method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name: str, value: Union[int, float]) -> None:
+        """Validate value as integer"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
 
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         """Initialize Rectangle"""
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
-    def area(self):
-        """Return the area of Rectangle"""
+    def area(self) -> int:
+        """Return the area ofthe  Rectangle"""
         return self.__width * self.__height
-
-    def __str__(self):
-        """Return Rectangle description"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
     """Square class"""
 
-    def __init__(self, size):
-        """Initialize Square"""
+    def __init__(self, size: int) -> None:
+        """Initialize the  Square"""
+        self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
 
-    def __str__(self):
-        """Return Square description"""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+    def area(self) -> int:
+        """Return the area of the  Square"""
+        return self.__size ** 2
