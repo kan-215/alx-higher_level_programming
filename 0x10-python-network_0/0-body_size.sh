68a5f9b2ee8c4fab3765bba2fab02b71f4f6639b
kan-215 <katamialvin369@gmail.com>
2024-07-04 23:09:50 +0300

    python-network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-curl -s "${1}" | wc -c
+#  Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | tr -d '\r' | cut -d ' ' -f2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# send the Get request to the url and display the body of response
+
+curl -sL "$1"
