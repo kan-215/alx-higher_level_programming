41b86be71ef0d457492afcf93528b7bcb4a46c43
kan-215 <katamialvin369@gmail.com>
2024-07-04 23:47:04 +0300

    python-network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL passed as the first argument
-URL="$1"
-
-# Sends a POST request to the passed URL and displays the body of the response
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "$URL"
+# The script sends a POST request to the passed URL, and displays the body of the response
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
