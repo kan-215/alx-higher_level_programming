0b2a44181b892ebea7648676dadc0e5b5b37a782
kan-215 <katamialvin369@gmail.com>
2024-07-04 23:38:07 +0300

    python-network

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""the script creates an Algorithm for a list of integers."""
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
